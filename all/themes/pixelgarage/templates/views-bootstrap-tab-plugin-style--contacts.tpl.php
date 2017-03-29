42f48bc23cad91a3bd5053d58acf95d4bd7b3d7f
Ralph Moser <ralph@ramosoft.ch>
2017-03-29 11:49:36 +0200

    - contacts hidden

--- all/themes/pixelgarage/templates/views-bootstrap-tab-plugin-style--contacts.tpl.php
+++ all/themes/pixelgarage/templates/views-bootstrap-tab-plugin-style--contacts.tpl.php
@@ -2,7 +2,7 @@
   <ul class="nav nav-<?php print $tab_type?> <?php if ($justified) print 'nav-justified' ?>">
     <?php $index = 0 ?>
     <?php foreach ($tabs as $location => $tab): ?>
-      <li class="<?php if ($index === 0) print 'active'?>">
+      <li class="<?php if ($index === 0) print ''?>">
         <a href="#tab-<?php print "{$id}-{$index}" ?>" data-toggle="tab"><?php print $location ?></a>
       </li>
       <?php $index++ ?>
@@ -11,7 +11,7 @@
   <div class="tab-content">
     <?php $index = 0 ?>
     <?php foreach ($tabs as $location => $row_indexes): ?>
-      <div class="tab-pane <?php if ($index === 0) print 'active'?>" id="tab-<?php print "{$id}-{$index}" ?>">
+      <div class="tab-pane <?php if ($index === 0) print ''?>" id="tab-<?php print "{$id}-{$index}" ?>">
         <?php foreach ($row_indexes as $row_index): ?>
           <div class="tab-pane-item contact">
             <?php print $rows[$row_index] ?>
