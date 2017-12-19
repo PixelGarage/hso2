5f1d8fc9110f8a64972307524694b5bde4907875
Ralph Moser <ralph@ramosoft.ch>
2017-12-19 17:59:46 +0100

    - leyla input styling implemented - sidebar views course dependant - footer with location image

--- all/themes/pixelgarage/templates/views-bootstrap-tab-plugin-style--locations.tpl.php
+++ all/themes/pixelgarage/templates/views-bootstrap-tab-plugin-style--locations.tpl.php
@@ -10,20 +10,21 @@
     <?php foreach ($locations as $key => $location): ?>
       <div class="tab-pane <?php if ($key === 0) print 'active'?>" id="tab-<?php print "{$id}-{$key}" ?>">
         <div class="row">
-          <div class="col-sm-6">
+          <div class="col-sm-4">
             <div class="organisation-block"><strong><?php print $location['organisation_name']; ?></strong></div>
             <div class="street-block"><?php print $location['thoroughfare']; ?></div>
             <div class="locality-block"><?php print $location['country']; ?>-<?php print $location['postal_code']; ?> <?php print $location['locality']; ?></div>
             <div class="phone-number"><span class="icon-phone"></span> <?php print $location['phone_number']; ?></div>
             <div class="email"><span class="icon-mail"></span> <?php print $location['email']; ?></div>
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-8">
             <!-- print location map -->
             <div class="map-location">
               <a href="https://maps.google.com/maps?q=<?php print $location['map_location']; ?>" target="_blank">
-                <img src="https://maps.googleapis.com/maps/api/staticmap?center=<?php print $location['map_location']; ?>&language=de&size=250x173&maptype=roadmap&markers=color:red%7C<?php print $location['map_location']; ?>&sensor=false" alt="" />
+                <img src="https://maps.googleapis.com/maps/api/staticmap?center=<?php print $location['map_location']; ?>&language=de&size=238x188&maptype=roadmap&markers=color:red%7C<?php print $location['map_location']; ?>&sensor=false" alt="" />
               </a>
             </div>
+            <div class="location-image"><?php print $location['picture']; ?></div>
           </div>
         </div>
       </div>
