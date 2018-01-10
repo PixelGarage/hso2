ce52ecf437c53e18afcc08bcec0fcd8ea467b086
Ralph Moser <ralph@ramosoft.ch>
2018-01-10 18:41:55 +0100

    - Reservation buttons invisible for Professional Programs

--- all/themes/pixelgarage/templates/views-view-field--course-times--nothing-1.tpl.php
+++ all/themes/pixelgarage/templates/views-view-field--course-times--nothing-1.tpl.php
@@ -7,6 +7,6 @@ $course = node_load($course_times->field_course[LANGUAGE_NONE][0]['target_id']);
 $segment = !empty($course->field_segment) ? $course->field_segment[LANGUAGE_NONE][0]['tid'] : 0;
 $start_date = $course_times->field_course_date[LANGUAGE_NONE][0]['value'];
 $start_timestamp = strtotime($start_date);
-$output = (($start_timestamp - $now) > 864000 && $segment != 160) ? l($output, 'node/' . $row->nid . '/reservation', array('html' => TRUE)) : '';
+$output = (($start_timestamp - $now) > 864000 && $segment != 160 && $segment != 164) ? l($output, 'node/' . $row->nid . '/reservation', array('html' => TRUE)) : '';
 ?>
 <?php print $output; ?>
